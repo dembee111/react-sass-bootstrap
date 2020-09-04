d69c7506ddb76a158c561b8bca857ad61e67e267
Dembee <minion.ck@gmail.com>
2020-09-04 21:43:29 +0800

    all done

--- src/App.js
+++ src/App.js
@@ -174,7 +174,7 @@ function App() {
         </div>
       </div>
 
-      <section className="py-8">
+      <section className="py-6 py-md-10">
         <div className="container-xl">
           <div className="row justify-content-center">
             <div className="col-5">
@@ -255,7 +255,503 @@ function App() {
           </div>
         </div>
       </section>
-      <section className="py-6"></section>
+      <section className="py-6 bg-gradient-1">
+        <div className="container-xl">
+          <div className="row">
+            <div className="col-12">
+              <h3>女套衫</h3>
+            </div>
+            <div className="col-md-3">
+              <div className="card-secondary">
+                <img
+                  src={require("./assets/img/third-card-1-2x.png")}
+                  className="img-fluid"
+                  alt="..."
+                />
+                <div className="card-secondary-title">
+                  <ul className="list-unstyled mb-0">
+                    <li>
+                      <h6 className="text-uppercase mb-0">Bravo neck dress</h6>
+                    </li>
+                    <li>
+                      <small className="text-black-50 mb-0">$300</small>
+                    </li>
+                    <li>
+                      <small className="text-black-50 mb-0">+6 Colors</small>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-3">
+              <div className="card-secondary">
+                <img
+                  src={require("./assets/img/third-card-2-2x.png")}
+                  className="img-fluid"
+                  alt="..."
+                />
+                <div className="card-secondary-title">
+                  <ul className="list-unstyled mb-0">
+                    <li>
+                      <h6 className="text-uppercase mb-0">Good neck dress</h6>
+                    </li>
+                    <li>
+                      <small className="text-black-50 mb-0">$250</small>
+                    </li>
+                    <li>
+                      <small className="text-black-50 mb-0">+7 Colors</small>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-3">
+              <div className="card-secondary">
+                <img
+                  src={require("./assets/img/third-card-3-2x.png")}
+                  className="img-fluid"
+                  alt="..."
+                />
+                <div className="card-secondary-title">
+                  <ul className="list-unstyled mb-0">
+                    <li>
+                      <h6 className="text-uppercase mb-0">Boat neck dress</h6>
+                    </li>
+                    <li>
+                      <small className="text-black-50 mb-0">$350</small>
+                    </li>
+                    <li>
+                      <small className="text-black-50 mb-0">+6 Colors</small>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-3">
+              <div className="card-secondary">
+                <img
+                  src={require("./assets/img/third-card-4-2x.png")}
+                  className="img-fluid"
+                  alt="..."
+                />
+                <div className="card-secondary-title">
+                  <ul className="list-unstyled mb-0">
+                    <li>
+                      <h6 className="text-uppercase mb-0">Nice neck dress</h6>
+                    </li>
+                    <li>
+                      <small className="text-black-50 mb-0">$360</small>
+                    </li>
+                    <li>
+                      <small className="text-black-50 mb-0">+6 Colors</small>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+          {/* Эрэгтэй хэсэг */}
+          <div className="row mt-6">
+            <div className="col-12">
+              <h3>女套衫</h3>
+            </div>
+            <div className="col-md-3">
+              <div className="card-secondary">
+                <img
+                  src={require("./assets/img/men-2-2x.png")}
+                  className="img-fluid"
+                  alt="..."
+                />
+                <div className="card-secondary-title">
+                  <ul className="list-unstyled mb-0">
+                    <li>
+                      <h6 className="text-uppercase mb-0">Bravo neck dress</h6>
+                    </li>
+                    <li>
+                      <small className="text-black-50 mb-0">$300</small>
+                    </li>
+                    <li>
+                      <small className="text-black-50 mb-0">+6 Colors</small>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-3">
+              <div className="card-secondary">
+                <img
+                  src={require("./assets/img/men-1-2x.png")}
+                  className="img-fluid"
+                  alt="..."
+                />
+                <div className="card-secondary-title">
+                  <ul className="list-unstyled mb-0">
+                    <li>
+                      <h6 className="text-uppercase mb-0">Good neck dress</h6>
+                    </li>
+                    <li>
+                      <small className="text-black-50 mb-0">$250</small>
+                    </li>
+                    <li>
+                      <small className="text-black-50 mb-0">+7 Colors</small>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-3">
+              <div className="card-secondary">
+                <img
+                  src={require("./assets/img/men-3-@2x.png")}
+                  className="img-fluid"
+                  alt="..."
+                />
+                <div className="card-secondary-title">
+                  <ul className="list-unstyled mb-0">
+                    <li>
+                      <h6 className="text-uppercase mb-0">Boat neck dress</h6>
+                    </li>
+                    <li>
+                      <small className="text-black-50 mb-0">$350</small>
+                    </li>
+                    <li>
+                      <small className="text-black-50 mb-0">+6 Colors</small>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-3">
+              <div className="card-secondary">
+                <img
+                  src={require("./assets/img/men-4-2x.png")}
+                  className="img-fluid"
+                  alt="..."
+                />
+                <div className="card-secondary-title">
+                  <ul className="list-unstyled mb-0">
+                    <li>
+                      <h6 className="text-uppercase mb-0">Nice neck dress</h6>
+                    </li>
+                    <li>
+                      <small className="text-black-50 mb-0">$360</small>
+                    </li>
+                    <li>
+                      <small className="text-black-50 mb-0">+6 Colors</small>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      {/* Нэг хүүхэнтэй хэсэг */}
+      <section className="py-6 py-md-10">
+        <div className="container-xl">
+          <div className="row justify-content-center">
+            <div className="col-5">
+              <h4 className="text-center">
+                春意弥漫空中！是的，现在您就可以穿着全新羊绒服上街了！具有轻盈柔和质感的#全新春夏系列，将在这个不一样的季节为您带来春夏独特的温暖和崭新体验
+              </h4>
+            </div>
+          </div>
+          <div className="row mt-5">
+            <div className="col-md-4">
+              <div className="card-primary">
+                <div className="card-overlay"></div>
+                <img
+                  src={require("./assets/img/primary-card.png")}
+                  className="img-fluid"
+                  alt="..."
+                />
+                <div className="card-primary-title">
+                  <h4 className="mb-0">裙装</h4>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-4">
+              <div className="card-primary">
+                <div className="card-overlay"></div>
+                <img
+                  src={require("./assets/img/primary-card-1.png")}
+                  className="img-fluid"
+                  alt="..."
+                />
+                <div className="card-primary-title">
+                  <h4 className="mb-0">套装</h4>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-4">
+              <div className="card-primary">
+                <div className="card-overlay"></div>
+                <img
+                  src={require("./assets/img/primary-card.png")}
+                  className="img-fluid"
+                  alt="..."
+                />
+                <div className="card-primary-title">
+                  <h4 className="mb-0">套衫</h4>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="row mt-5">
+            <div className="col-md-6">
+              <div className="card-primary">
+                <div className="card-overlay"></div>
+                <img
+                  src={require("./assets/img/second-card-1.png")}
+                  className="img-fluid"
+                  alt="..."
+                />
+                <div className="card-primary-title">
+                  <h4 className="mb-0">圆领</h4>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-6">
+              <div className="card-primary">
+                <div className="card-overlay"></div>
+                <img
+                  src={require("./assets/img/second-card-2.jpg")}
+                  className="img-fluid"
+                  alt="..."
+                />
+                <div className="card-primary-title">
+                  <h4 className="mb-0">V-领</h4>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="py-6 bg-gradient-1">
+        <div className="container-xl">
+          <div className="row">
+            <div className="col-12">
+              <h3>女套衫</h3>
+            </div>
+            <div className="col-md-3">
+              <div className="card-secondary">
+                <img
+                  src={require("./assets/img/third-card-1-2x.png")}
+                  className="img-fluid"
+                  alt="..."
+                />
+                <div className="card-secondary-title">
+                  <ul className="list-unstyled mb-0">
+                    <li>
+                      <h6 className="text-uppercase mb-0">Bravo neck dress</h6>
+                    </li>
+                    <li>
+                      <small className="text-black-50 mb-0">$300</small>
+                    </li>
+                    <li>
+                      <small className="text-black-50 mb-0">+6 Colors</small>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-3">
+              <div className="card-secondary">
+                <img
+                  src={require("./assets/img/third-card-2-2x.png")}
+                  className="img-fluid"
+                  alt="..."
+                />
+                <div className="card-secondary-title">
+                  <ul className="list-unstyled mb-0">
+                    <li>
+                      <h6 className="text-uppercase mb-0">Good neck dress</h6>
+                    </li>
+                    <li>
+                      <small className="text-black-50 mb-0">$250</small>
+                    </li>
+                    <li>
+                      <small className="text-black-50 mb-0">+7 Colors</small>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-3">
+              <div className="card-secondary">
+                <img
+                  src={require("./assets/img/third-card-3-2x.png")}
+                  className="img-fluid"
+                  alt="..."
+                />
+                <div className="card-secondary-title">
+                  <ul className="list-unstyled mb-0">
+                    <li>
+                      <h6 className="text-uppercase mb-0">Boat neck dress</h6>
+                    </li>
+                    <li>
+                      <small className="text-black-50 mb-0">$350</small>
+                    </li>
+                    <li>
+                      <small className="text-black-50 mb-0">+6 Colors</small>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-3">
+              <div className="card-secondary">
+                <img
+                  src={require("./assets/img/third-card-4-2x.png")}
+                  className="img-fluid"
+                  alt="..."
+                />
+                <div className="card-secondary-title">
+                  <ul className="list-unstyled mb-0">
+                    <li>
+                      <h6 className="text-uppercase mb-0">Nice neck dress</h6>
+                    </li>
+                    <li>
+                      <small className="text-black-50 mb-0">$360</small>
+                    </li>
+                    <li>
+                      <small className="text-black-50 mb-0">+6 Colors</small>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+          {/* Эрэгтэй хэсэг */}
+          <div className="row mt-6">
+            <div className="col-12">
+              <h3>女套衫</h3>
+            </div>
+            <div className="col-md-3">
+              <div className="card-secondary">
+                <img
+                  src={require("./assets/img/men-2-2x.png")}
+                  className="img-fluid"
+                  alt="..."
+                />
+                <div className="card-secondary-title">
+                  <ul className="list-unstyled mb-0">
+                    <li>
+                      <h6 className="text-uppercase mb-0">Bravo neck dress</h6>
+                    </li>
+                    <li>
+                      <small className="text-black-50 mb-0">$300</small>
+                    </li>
+                    <li>
+                      <small className="text-black-50 mb-0">+6 Colors</small>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-3">
+              <div className="card-secondary">
+                <img
+                  src={require("./assets/img/men-1-2x.png")}
+                  className="img-fluid"
+                  alt="..."
+                />
+                <div className="card-secondary-title">
+                  <ul className="list-unstyled mb-0">
+                    <li>
+                      <h6 className="text-uppercase mb-0">Good neck dress</h6>
+                    </li>
+                    <li>
+                      <small className="text-black-50 mb-0">$250</small>
+                    </li>
+                    <li>
+                      <small className="text-black-50 mb-0">+7 Colors</small>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-3">
+              <div className="card-secondary">
+                <img
+                  src={require("./assets/img/men-3-@2x.png")}
+                  className="img-fluid"
+                  alt="..."
+                />
+                <div className="card-secondary-title">
+                  <ul className="list-unstyled mb-0">
+                    <li>
+                      <h6 className="text-uppercase mb-0">Boat neck dress</h6>
+                    </li>
+                    <li>
+                      <small className="text-black-50 mb-0">$350</small>
+                    </li>
+                    <li>
+                      <small className="text-black-50 mb-0">+6 Colors</small>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-3">
+              <div className="card-secondary">
+                <img
+                  src={require("./assets/img/men-4-2x.png")}
+                  className="img-fluid"
+                  alt="..."
+                />
+                <div className="card-secondary-title">
+                  <ul className="list-unstyled mb-0">
+                    <li>
+                      <h6 className="text-uppercase mb-0">Nice neck dress</h6>
+                    </li>
+                    <li>
+                      <small className="text-black-50 mb-0">$360</small>
+                    </li>
+                    <li>
+                      <small className="text-black-50 mb-0">+6 Colors</small>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className="py-md-10">
+        <div className="container-xl">
+          <div className="row justify-content-around align-items-center">
+            <div className="col-md-3">
+              <div className="blue-outline">
+                <img
+                  src={require("./assets/img/outlinetai.png")}
+                  className="img-fluid"
+                  alt="..."
+                />
+              </div>
+            </div>
+
+            <div className="col-md-8">
+              <div className="card-side">
+                <img
+                  src={require("./assets/img/zuragtai-huuhen.png")}
+                  className="img-fluid"
+                  alt="..."
+                />
+                <div className="card-side-title">
+                  <h1 className="mb-0">初秋 焕新</h1>
+                  {/* <div className="abs-center">
+                    
+                  </div> */}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
