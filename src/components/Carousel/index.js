7a06bee5bf8c83014428d4f85a419f8d931c002b
Dembee <minion.ck@gmail.com>
2020-09-07 12:59:06 +0800

    all done

--- src/components/Carousel/index.js
+++ src/components/Carousel/index.js
@@ -1,11 +1,7 @@
 import React from "react";
 import Slider from "react-slick";
 import imageUrlBackup1 from "../../assets/img/secondary-photo-backup.png";
-import imageForBigSize1 from "../../assets/img/secondary-photo.webp";
-import imageForSmallSize1 from "../../assets/img/secondary-photo-1x.webp";
 import imageUrlBackup from "../../assets/img/Header-photo-backup.png";
-import imageForBigSize from "../../assets/img/Header-photo.webp";
-import imageForSmallSize from "../../assets/img/Header-photo-1x.webp";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Arrow from "./Arrow";
@@ -29,16 +25,13 @@ export default function Carousel() {
         prevArrow={<Arrow type="prev" />}
       >
         <picture>
-          <source media="(max-width: 799px)" srcset={imageForSmallSize} />
-          <source media="(min-width: 1600px)" srcset={imageForBigSize} />
-          <img srcset={imageUrlBackup} alt="header photo 1" width="100%" />
+          <source media="(max-width: 799px)" srcset={imageUrlBackup} />
+          <img srcset={imageUrlBackup} alt="slider-1" width="100%" />
         </picture>
         <picture>
-          <source media="(max-width: 799px)" srcset={imageForSmallSize1} />
-          <source media="(min-width: 1600px)" srcset={imageForBigSize1} />
-          <img srcset={imageUrlBackup1} alt="header photo 1" width="100%" />
+          <source media="(max-width: 799px)" srcset={imageUrlBackup1} />
+          <img srcset={imageUrlBackup1} alt="slider-2" width="100%" />
         </picture>
-        {/* <img src={imageUrl2} width="100%" alt="sliderImg" /> */}
       </Slider>
     </div>
   );
